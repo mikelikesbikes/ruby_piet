5293860a180429681a314a033aecbafb10debcdd
Mike Busch <busch.michael@gmail.com>
2010-12-18 16:03:30 -0600

    split Color into its own module

--- lib/codel.rb
+++ lib/codel.rb
@@ -7,49 +7,50 @@ module Piet
     end
 
     def distance(codel)
-      self_pos = COLOR_POSITION[self]
-      codel_pos = COLOR_POSITION[codel]
+      self_pos = Colors::POSITION[self]
+      codel_pos = Colors::POSITION[codel]
       lightness_change = (codel_pos[0] - self_pos[0]).modulo(3)
       hue_change = (codel_pos[1] - self_pos[1]).modulo(6)
       [hue_change, lightness_change]
     end
 
-    def self.darken(color)
-      color & COLOR_MASK
-    end
+    module Colors
+      def self.darken(color)
+        color & MASK
+      end
 
-    def self.lighten(color)
-      color | COLOR_MASK
-    end
+      def self.lighten(color)
+        color | MASK
+      end
 
-    def self.int_value(hex)
-      hex.to_i(16)
-    end
+      def self.int_value(hex)
+        hex.to_i(16)
+      end
 
-    COLOR_MASK = int_value("C0C0C0FF")
-
-    COLOR_POSITION = {}
-    COLOR_MAP = {}
-    { :RED => "FF0000FF",
-      :YELLOW => "FFFF00FF",
-      :GREEN => "00FF00FF",
-      :CYAN => "00FFFFFF",
-      :BLUE => "0000FFFF",
-      :MAGENTA => "FF00FFFF" }.each_with_index do |(name, value), index|
-      color  = int_value(value)
-      normal = self.const_set(name, self.new(color))
-      light  = self.const_set("LIGHT_#{name}", self.new(lighten(color)))
-      dark   = self.const_set("DARK_#{name}", self.new(darken(color)))
-      COLOR_POSITION[normal] = [1, index]
-      COLOR_POSITION[light]  = [0, index]
-      COLOR_POSITION[dark]   = [2, index]
-      COLOR_MAP[normal.color] = normal
-      COLOR_MAP[light.color] = light
-      COLOR_MAP[dark.color] = dark
+      MASK = int_value("C0C0C0FF")
+      POSITION = {}
+      MAP = {}
+      { :RED => "FF0000FF",
+        :YELLOW => "FFFF00FF",
+        :GREEN => "00FF00FF",
+        :CYAN => "00FFFFFF",
+        :BLUE => "0000FFFF",
+        :MAGENTA => "FF00FFFF" }.each_with_index do |(name, value), index|
+        color  = int_value(value)
+        normal = self.const_set(name, Piet::Codel.new(color))
+        light  = self.const_set("LIGHT_#{name}", Piet::Codel.new(lighten(color)))
+        dark   = self.const_set("DARK_#{name}", Piet::Codel.new(darken(color)))
+        POSITION[normal] = [1, index]
+        POSITION[light]  = [0, index]
+        POSITION[dark]   = [2, index]
+        MAP[normal.color] = normal
+        MAP[light.color] = light
+        MAP[dark.color] = dark
+      end
+      BLACK = Piet::Codel.new(int_value("000000FF"))
+      MAP[BLACK.color] = BLACK
+      WHITE = Piet::Codel.new(int_value("FFFFFFFF"))
+      MAP[WHITE.color] = WHITE
     end
-    BLACK = self.new(int_value("000000FF"))
-    COLOR_MAP[BLACK.color] = BLACK
-    WHITE = self.new(int_value("FFFFFFFF"))
-    COLOR_MAP[WHITE.color] = WHITE
   end
 end
--- spec/codel_spec.rb
+++ spec/codel_spec.rb
@@ -1,81 +1,84 @@
 require 'spec_helper'
 
 describe Piet::Codel do
+  include Piet::Codel::Colors
+
   def int_value(hex)
     hex.to_i(16)
   end
+
   describe "color constants" do
-    specify { Piet::Codel::RED.color.should ==            int_value("FF0000FF") }
-    specify { Piet::Codel::LIGHT_RED.color.should ==      int_value("FFC0C0FF") }
-    specify { Piet::Codel::DARK_RED.color.should ==       int_value("C00000FF") }
-    specify { Piet::Codel::YELLOW.color.should ==         int_value("FFFF00FF") }
-    specify { Piet::Codel::LIGHT_YELLOW.color.should ==   int_value("FFFFC0FF") }
-    specify { Piet::Codel::DARK_YELLOW.color.should ==    int_value("C0C000FF") }
-    specify { Piet::Codel::GREEN.color.should ==          int_value("00FF00FF") }
-    specify { Piet::Codel::LIGHT_GREEN.color.should ==    int_value("C0FFC0FF") }
-    specify { Piet::Codel::DARK_GREEN.color.should ==     int_value("00C000FF") }
-    specify { Piet::Codel::CYAN.color.should ==           int_value("00FFFFFF") }
-    specify { Piet::Codel::LIGHT_CYAN.color.should ==     int_value("C0FFFFFF") }
-    specify { Piet::Codel::DARK_CYAN.color.should ==      int_value("00C0C0FF") }
-    specify { Piet::Codel::BLUE.color.should ==           int_value("0000FFFF") }
-    specify { Piet::Codel::LIGHT_BLUE.color.should ==     int_value("C0C0FFFF") }
-    specify { Piet::Codel::DARK_BLUE.color.should ==      int_value("0000C0FF") }
-    specify { Piet::Codel::MAGENTA.color.should ==        int_value("FF00FFFF") }
-    specify { Piet::Codel::LIGHT_MAGENTA.color.should ==  int_value("FFC0FFFF") }
-    specify { Piet::Codel::DARK_MAGENTA.color.should ==   int_value("C000C0FF") }
-    specify { Piet::Codel::WHITE.color.should ==          int_value("FFFFFFFF") }
-    specify { Piet::Codel::BLACK.color.should ==          int_value("000000FF") }
+    specify { RED.color.should ==           int_value("FF0000FF") }
+    specify { LIGHT_RED.color.should ==     int_value("FFC0C0FF") }
+    specify { DARK_RED.color.should ==      int_value("C00000FF") }
+    specify { YELLOW.color.should ==        int_value("FFFF00FF") }
+    specify { LIGHT_YELLOW.color.should ==  int_value("FFFFC0FF") }
+    specify { DARK_YELLOW.color.should ==   int_value("C0C000FF") }
+    specify { GREEN.color.should ==         int_value("00FF00FF") }
+    specify { LIGHT_GREEN.color.should ==   int_value("C0FFC0FF") }
+    specify { DARK_GREEN.color.should ==    int_value("00C000FF") }
+    specify { CYAN.color.should ==          int_value("00FFFFFF") }
+    specify { LIGHT_CYAN.color.should ==    int_value("C0FFFFFF") }
+    specify { DARK_CYAN.color.should ==     int_value("00C0C0FF") }
+    specify { BLUE.color.should ==          int_value("0000FFFF") }
+    specify { LIGHT_BLUE.color.should ==    int_value("C0C0FFFF") }
+    specify { DARK_BLUE.color.should ==     int_value("0000C0FF") }
+    specify { MAGENTA.color.should ==       int_value("FF00FFFF") }
+    specify { LIGHT_MAGENTA.color.should == int_value("FFC0FFFF") }
+    specify { DARK_MAGENTA.color.should ==  int_value("C000C0FF") }
+    specify { WHITE.color.should ==         int_value("FFFFFFFF") }
+    specify { BLACK.color.should ==         int_value("000000FF") }
   end
 
   describe "distance" do
 
     context "same hue and lightness" do
       it "should return [0, 0] for the same color" do
-        Piet::Codel::RED.distance(Piet::Codel::RED).should == [0, 0]
+        RED.distance(RED).should == [0, 0]
       end
     end
 
     context "same hue" do
       it "should return [0, 1] for 1 step in darker" do
-        Piet::Codel::LIGHT_RED.distance(Piet::Codel::RED).should == [0, 1]
+        LIGHT_RED.distance(RED).should == [0, 1]
       end
       it "should return [0, 2] for 2 step in darker" do
-        Piet::Codel::LIGHT_RED.distance(Piet::Codel::DARK_RED).should == [0, 2]
+        LIGHT_RED.distance(DARK_RED).should == [0, 2]
       end
       it "should return [0, 2] for 1 step in darker (cycle)" do
-        Piet::Codel::DARK_RED.distance(Piet::Codel::LIGHT_RED).should == [0, 1]
+        DARK_RED.distance(LIGHT_RED).should == [0, 1]
       end
     end
 
     context "same lightness" do
       it "should return [1, 0] for 1 step in hue" do
-        Piet::Codel::RED.distance(Piet::Codel::YELLOW).should == [1, 0]
+        RED.distance(YELLOW).should == [1, 0]
       end
       it "should return [2, 0] for 2 steps in hue" do
-        Piet::Codel::YELLOW.distance(Piet::Codel::CYAN).should == [2, 0]
+        YELLOW.distance(CYAN).should == [2, 0]
       end
       it "should should cycle" do
-        Piet::Codel::MAGENTA.distance(Piet::Codel::RED).should == [1, 0]
+        MAGENTA.distance(RED).should == [1, 0]
       end
       it "should return [5, 0] for 5 steps in hue" do
-        Piet::Codel::YELLOW.distance(Piet::Codel::RED).should == [5, 0]
+        YELLOW.distance(RED).should == [5, 0]
       end
     end
 
     context "both different" do
       it "should return [1, 1] for 1 step in hue and lightness" do
-        Piet::Codel::RED.distance(Piet::Codel::DARK_YELLOW).should == [1, 1]
+        RED.distance(DARK_YELLOW).should == [1, 1]
       end
 
       it "should return [1, 2] for 1 step in hue and 2 in lightness" do
-        Piet::Codel::LIGHT_RED.distance(Piet::Codel::DARK_YELLOW).should == [1, 2]
+        LIGHT_RED.distance(DARK_YELLOW).should == [1, 2]
       end
 
       it "should return [5, 2] for 5 steps in hue and 2 in lightness" do
-        Piet::Codel::LIGHT_RED.distance(Piet::Codel::DARK_MAGENTA).should == [5, 2]
+        LIGHT_RED.distance(DARK_MAGENTA).should == [5, 2]
       end
       it "should return [1, 2] for 1 steps in hue and 2 in lightness" do
-        Piet::Codel::LIGHT_MAGENTA.distance(Piet::Codel::DARK_RED).should == [1, 2]
+        LIGHT_MAGENTA.distance(DARK_RED).should == [1, 2]
       end
     end
   end
